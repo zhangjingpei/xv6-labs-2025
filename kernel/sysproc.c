c3c2929da71450e4cc5f2d62cb1209dd90858ad2
zhangjingpei <1005012954@qq.com>
2025-07-15 22:06:01 +0800

    backtrace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -52,10 +52,12 @@ uint64 sys_sleep(void)
     int n;
     uint ticks0;
 
+    backtrace();
+
     if (argint(0, &n) < 0)
         return -1;
     acquire(&tickslock);
-    ticks0 = ticks;
+    ticks0 = ticks; 
     while (ticks - ticks0 < n)
     {
         if (myproc()->killed)
