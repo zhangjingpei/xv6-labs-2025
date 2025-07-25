fb2fda448292d7cb4f1c324b2f5f116cbd2ee4fb
zhangjingpei <1005012954@qq.com>
2025-07-25 21:33:17 +0800

    错误

--- kernel/fcntl.h
+++ kernel/fcntl.h
@@ -1,3 +1,4 @@
+#define LAB_MMAP
 #define O_RDONLY  0x000
 #define O_WRONLY  0x001
 #define O_RDWR    0x002
--- user/user.h
+++ user/user.h
@@ -1,6 +1,7 @@
 struct stat;
 struct rtcdate;
 
+
 // system calls
 int fork(void);
 int exit(int) __attribute__((noreturn));
@@ -23,6 +24,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+void * mmap(void * addr, int len, int prot, int flags, int fd, uint offset);
+int munmap(void * addr, int len);
 
 // ulib.c
 int stat(const char*, struct stat*);
